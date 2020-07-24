852f8cca1ea2babed44e81bd76c8274565b53db4
VedranSagodic <vedran.sagodic@gmail.com>
2020-07-24 11:10:46 +0200

    Added meta tags

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php 
-  require_once 'config.php';
+require_once 'config.php';
 
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
+<html prefix="og: https://ogp.me/ns#">
   <head profile="https://polaznik13.edunova.hr/app_foundation.hr/index.php">
-    <link rel="icon" 
+<link rel="icon" 
       type="image/png" 
       href="https://polaznik13.edunova.hr/fav-icon/fav-icon-16x16.png">
-
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property=”og:type” content=”website” />
     <title>Properties App</title>
+
+    <meta property="og:title" content="Properties App" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://polaznik13.edunova.hr/app_foundation.hr/index.php" />
 
     <link rel="stylesheet" href="assets/css/foundation.css">
     <link rel="stylesheet" href="assets/css/form.css">
@@ -37,46 +41,44 @@
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
   </head>
-    <body>
+<body>
 
-      <h2>Properties app</h2>
-      <h4>
-        <?php
-          $p=isset($_GET['p']) ? (int)$_GET['p'] : 0;
-          switch ($p) {
-            case 1:
-              echo 'Error: Input parameters';
-              break;
-            case 2:
-              echo 'Please sign in with your e-mail and password';
-              break;
-            case 3:
-              echo 'Wrong email and/or password.';
-              break;
-            default:
-              echo 'Please sign in.';
-              break;
-          }
-        ?>
-      </h4>
+  <h2>Properties app</h2>
+  <h4>
+    <?php
 
-      <form class="log-in-form" action="autorization.php" method="POST">
+$p=isset($_GET['p']) ? (int)$_GET['p'] : 0;
 
-        <label>Email
-          <input type="email" id="email" name="email" placeholder="Email">
-        </label>
+switch ($p) {
+  case 1:
+    echo 'Error: Input parameters';
+    break;
+  case 2:
+    echo 'Please sign in with your e-mail and password';
+    break;
+  case 3:
+    echo 'Wrong email and/or password.';
+    break;
+  default:
+    echo 'Please sign in.';
+    break;
+}
 
-        <label>Password
-          <input type="password" id="pass" name="password" placeholder="Password">
-        </label>
+?>
+</h4>
+<form class="log-in-form" action="autorization.php" method="POST">
+  <label>Email
+  <input type="email" id="email" name="email" placeholder="Email">
+  </label>
+  <label>Password
+  <input type="password" id="pass" name="password" placeholder="Password">
+  </label>
+  <input class="button expanded" type="submit" value="Sign in">
+</form>
 
-          <input class="button expanded" type="submit" value="Sign in">
-      </form>
+<a href="https://github.com/VedranSagodic/PropertiesApp_foundation">Link na github kod</a>
 
-          <a href="https://github.com/VedranSagodic/PropertiesApp_foundation/tree/master">Link na github kod</a>
-
-
-    <script src="assets/js/vendor.js"></script>
-    <script src="assets/js/foundation.js"></script>
-    </body>
+<script src="assets/js/vendor.js"></script>
+<script src="assets/js/foundation.js"></script>
+</body>
 </html>
